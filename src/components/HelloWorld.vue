1e6636aed7e29b401699f2e8de77208e3fd140f3
RauziDssh <rauzi.univ@gmail.com>
2018-07-04 05:12:51 +0900

    進捗

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="hello">
-    <div style="margin-top: 15px;">
-      <el-input placeholder="Please input folder id" v-model="input0" class="input-with-select">
-        <el-button slot="append" icon="el-icon-search" v-on:click="startSearch"></el-button>
-      </el-input>
+  1txD0R34T5sdkMh9KJK0LpUqjjyIGP6_R
+    <div style="margin-top: 1vh;">            
+        <el-input placeholder="Please input folder id" v-model="input0" class="input-with-select">
+            <template slot="prepend">
+              <img src="../assets/drive-icon-gray.png" style="height: 50%; margin:auto;">
+            </template>
+            <el-button slot="append" icon="el-icon-search" v-on:click="startSearch"></el-button>
+        </el-input>
+    </div>
+    <div style="margin-top: 1vh;">            
+        <el-input placeholder="Please input spreadsheet id" v-model="input1" class="input-with-select">
+            <template slot="prepend">
+              <img src="../assets/spreadsheet-icon-gray.png" style="height: 50%; margin:auto;">
+            </template>
+            <el-button slot="append" icon="el-icon-search" v-on:click="querySheet"></el-button>
+        </el-input>
     </div>
     <div class="custom-tree-container" style="margin-top:2vh;">
       <div class="block">
@@ -15,16 +27,24 @@
           @node-click="handleNodeClick"
           :expand-on-click-node="false"
         >
+
         <span class="custom-tree-node" slot-scope="{ node, data }">
+          
           <template v-if="node.label.isFolder === true">
             <span><i class="el-icon-goods"></i></span>
-            <span>{{ node.label["Folder Name"] }}</span>
+            <span>
+              {{ node.label["Folder Name"] }}
+            </span>
             <span style='margin-right: 5%;'></span>
             <span><i class="el-icon-goods" style="color:gray;"></i></span>
             <span>{{ node.label["foldercnt"] }}</span>
             <span><i class="el-icon-document" style="color:gray;"></i></span>
             <span>{{ node.label["itemcnt"] }}</span>
-            <span></span>
+            <span>
+              <el-tooltip v-bind:content="node.label.URL" placement="top">
+                <i class="el-icon-view" v-on:click="handleViewClick(node)"></i>
+              </el-tooltip>
+            </span>
             <span v-if="node.label.isEpmty === true">
               <i class="el-icon-warning" style="color:red;"> Empty</i>
             </span>
@@ -32,6 +52,12 @@
           <template v-else>
             <span><i class="el-icon-document"></i></span>
             <span>{{ node.label["Item Name"] }}</span>
+            <span style='margin-right: 5%;'></span>
+            <span>
+              <el-tooltip v-bind:content="node.label.URL" placement="top">
+                <i class="el-icon-view" v-on:click="handleViewClick(node)"></i>
+              </el-tooltip>
+            </span>
           </template>
         </span>
         </el-tree>
@@ -53,6 +79,7 @@ function makeTreeStructure(tdata) {
     item.isFolder = true
     item.foldercnt = childobj['foldercnt']
     item.itemcnt = childobj['itemcnt']
+    
     if(item.foldercnt + item.itemcnt == 0){
       item.isEpmty = true
     }else{
@@ -97,11 +124,23 @@ export default {
       data: [{label:"N/A"}],
       loading : false,
       input0: '',
+      input1: '',
     }
   },
   methods: {
       handleNodeClick(data) {
-        console.log(data);
+
+      },
+      handleViewClick(data) {
+        console.log(data)
+        var url = ""
+        if(data.label.isFolder){
+          url = data.label['URL'];
+        }else{
+          url = data.label['URL'];
+        }
+
+        window.open(url);
       },
       startSearch(event) {
         var surl = 'https://script.google.com/a/mis.doshisha.ac.jp/macros/s/AKfycbyoA-cfZH5dQRltGxIL1SNdiFg9DTRi57Zv81-K1qgP/dev?id=' + this.input0;
@@ -111,6 +150,20 @@ export default {
               // Success.
               console.log(json)
               this.data = makeTreeStructure(json).holder
+              this.loading = false;
+            }).catch(err => {
+              // Failed.
+              console.log(err)
+              this.loading = false;
+            })
+      },
+      querySheet(event) {
+        var baseurl = 'https://script.google.com/a/mis.doshisha.ac.jp/macros/s/AKfycbzwieN06w3RSngiNLxhQ6CCKepyKBYOpe1cwXhfhty4zk7wPJI/exec?id=' + this.input1;
+        this.loading = true;
+        this.$jsonp(baseurl).then(json => {
+              // Success.
+              console.log(json)
+              //this.data = makeTreeStructure(json).holder
               this.loading = false;
             }).catch(err => {
               // Failed.
